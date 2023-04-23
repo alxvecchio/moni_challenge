158363896974b1bc35d835e72e586c09e067c273
Alex Vecchio <avecchio@ASC.IT.PCS.I1624>
2023-04-23 15:03:23 -0300

    feat(moni): se añade vista home

--- home/views.py
+++ home/views.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 """Module for views."""
 
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request) -> HttpResponse:
+    """Handle Home view and its requests.
+
+    Args:
+        request (HttpRequest): HTTP Request.
+
+    Returns:
+        HttpResponse: Renders home.html template with the given context.
+    """
+    context: dict = {}
+    return render(request, "home.html", context)
