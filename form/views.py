f9c1ed2c493c062b80d31f3dd30cd63d2f01d5ca
Alex Vecchio <avecchio@ASC.IT.PCS.I1624>
2023-04-24 02:54:42 -0300

    feat(moni): (solo debug) primeras pruebas en la creacion del formulario

--- form/views.py
+++ form/views.py
@@ -3,4 +3,24 @@
 
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+import logging
+
+logger = logging.getLogger("prestamo_facil")
+
+
+def form(request) -> HttpResponse:
+    """Handle Home view and its requests.
+
+    Args:
+        request (HttpRequest): HTTP Request.
+
+    Returns:
+        HttpResponse: Renders home.html template with the given context.
+    """
+    try:
+        logger.info(f'Entering form from {request.META["REMOTE_ADDR"]}')
+        context: dict = {}
+        return render(request, "form.html", context)
+    except Exception as e:
+        logger.error(f"Error: {e}")
